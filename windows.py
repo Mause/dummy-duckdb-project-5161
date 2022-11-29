d05a7c9626cf356b0091c52f4f03dfa95ef9e6c0
Elliana May <me@mause.me>
2022-11-29 18:54:16 +0800

    Update windows.py

--- windows.py
+++ windows.py
@@ -8,9 +8,11 @@ download_object = multithread.Downloader(url, 'lineitem.parquet')
 download_object.start()
 
 con = duckdb.connect()
-result =con.execute(
+result = con.execute(
 '''
 SET memory_limit='10GB';
+SET temporary_dir ='.';
 select *,extract ( year from l_shipdate) as year  from 'lineitem.parquet' order by l_shipdate
 ''').fetch_record_batch()
+print('fetched')
 ds.write_dataset(result,"out_lineitem", format="parquet" , partitioning=['year'],partitioning_flavor="hive",max_rows_per_group=120000,existing_data_behavior="overwrite_or_ignore")

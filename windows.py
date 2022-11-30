d0978b3c49f33811025e9a66881112db7475313f
Elliana May <me@mause.me>
2022-11-30 10:34:32 +0800

    Update windows.py

--- windows.py
+++ windows.py
@@ -1,10 +1,9 @@
 import pyarrow.dataset as ds
 import duckdb
 import multithread
-import tracemalloc
 from github_action_utils import group
 
-tracemalloc.start()
+faulthandler.enable()
 
 with group('download'):
   multithread.Downloader('https://drive.google.com/uc?export=download&id=18gv0Yd_a-Zc7CSolol8qeYVAAzSthnSN&confirm=t', 'lineitem.parquet').start()
@@ -22,11 +21,6 @@ with group('query'):
     ''').fetch_record_batch()
   except duckdb.IOException as e:
     print(e)
-    snapshot = tracemalloc.take_snapshot()
-    top_stats = snapshot.statistics('lineno')
-    print("[ Top 10 ]")
-    for stat in top_stats[:10]:
-        print(stat)
     raise
 
 with group('write'):

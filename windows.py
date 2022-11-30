2f3c93a1064d94076705ed07e49dfc38697a235a
Elliana May <me@mause.me>
2022-11-30 10:14:35 +0800

    fail gracefully

--- windows.py
+++ windows.py
@@ -1,19 +1,33 @@
 import pyarrow.dataset as ds
 import duckdb
-
-url = 'https://drive.google.com/uc?export=download&id=18gv0Yd_a-Zc7CSolol8qeYVAAzSthnSN&confirm=t'
 import multithread
+import tracealloc
+from github_action_utils import group
 
-download_object = multithread.Downloader(url, 'lineitem.parquet')
-download_object.start()
+tracealloc.start()
+
+with group('download'):
+  multithread.Downloader('https://drive.google.com/uc?export=download&id=18gv0Yd_a-Zc7CSolol8qeYVAAzSthnSN&confirm=t', 'lineitem.parquet').start()
 
 con = duckdb.connect()
 con.execute('SET enable_progress_bar = true')
 #con.execute("SET memory_limit='10GB';")
 con.execute("SET temp_directory = '.';")
-result = con.execute(
-'''
-select *,extract ( year from l_shipdate) as year  from 'lineitem.parquet' order by l_shipdate
-''').fetch_record_batch()
-print('fetched')
-ds.write_dataset(result,"out_lineitem", format="parquet" , partitioning=['year'],partitioning_flavor="hive",max_rows_per_group=120000,existing_data_behavior="overwrite_or_ignore")
+
+with group('query'):
+  try:
+    result = con.execute(
+    '''
+    select *,extract ( year from l_shipdate) as year  from 'lineitem.parquet' order by l_shipdate
+    ''').fetch_record_batch()
+  except duckdb.IOException as e:
+    print(e)
+    snapshot = tracemalloc.take_snapshot()
+    top_stats = snapshot.statistics('lineno')
+    print("[ Top 10 ]")
+    for stat in top_stats[:10]:
+        print(stat)
+    raise
+
+with group('write'):
+  ds.write_dataset(result,"out_lineitem", format="parquet" , partitioning=['year'],partitioning_flavor="hive",max_rows_per_group=120000,existing_data_behavior="overwrite_or_ignore")
